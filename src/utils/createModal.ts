d13c8f2ff1ef5729b8a140d855efaa7c61d50ef2
Gabriel Gonçalves <103828336+GabrielFleckl@users.noreply.github.com>
2024-10-21 05:45:42 -0300

    fix remove useless import

--- src/utils/createModal.ts
+++ src/utils/createModal.ts
@@ -4,7 +4,6 @@ import {
   BrowserWindow
 
 } from "electron";
-import { is } from "@electron-toolkit/utils";
 
 function createModal(hash: string, parent: BrowserWindow) {
   const modalWindow = new BrowserWindow({
